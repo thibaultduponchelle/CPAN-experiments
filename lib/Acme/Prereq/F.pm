54bbc2f832615da14826a0214e29c8d759af86b5
Thibault DUPONCHELLE <thibault.duponchelle@amadeus.com>
2021-05-15 18:26:16 +0200

    Initial commit

---
+++ lib/Acme/Prereq/F.pm
@@ -0,0 +1,23 @@
+package Acme::Prereq::F;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Acme::Prereq::F - Module for testing CPAN module prerequisites
+
+=head1 VERSION
+
+Version 1.0.0 
+
+=cut
+
+our $VERSION = '1.0.0';
+
+sub one() {
+	print "One !\n";
+}
+
+1; # End of Acme::Prereq::F
