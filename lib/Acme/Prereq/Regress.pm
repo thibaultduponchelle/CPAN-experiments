723bf44d5cbcdf04ad9f1eb73aa7704cb5b2217c
Thibault DUPONCHELLE <thibault.duponchelle@amadeus.com>
2021-05-31 17:04:55 +0200

    Minor edits

--- lib/Acme/Prereq/Regress.pm
+++ lib/Acme/Prereq/Regress.pm
@@ -8,18 +8,8 @@ use warnings;
 
 Acme::Prereq::Regress - Module for testing CPAN Pause indexing
 
-=head1 VERSION
-
-Version 0.01
-
 =cut
 
 our $VERSION = '5.00';
-
-=head1 AUTHOR
-
-Thibault Duponchelle, C<< <thibault.duponchelle at gmail.com> >>
-
-=cut
 
 1; # End of Acme::Prereq::Regress
