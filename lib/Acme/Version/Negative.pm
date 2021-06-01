4ce27eb2a78dade8966457ae07ffc1d515c24537
Thibault DUPONCHELLE <thibault.duponchelle@amadeus.com>
2021-06-01 10:07:05 +0200

    Version -1.00

---
+++ lib/Acme/Version/Negative.pm
@@ -0,0 +1,19 @@
+package Acme::Version::Negative;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Acme::Version::Negative - A module to test CPAN Pause indexing
+
+=head1 VERSION
+
+Version -1.00
+
+=cut
+
+our $VERSION = '-1.00';
+
+1; # End of Acme::Version::Negative
