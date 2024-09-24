bef57e93808ec29f805221f78ea61e7094aea1c4
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2024-09-24 16:25:10 +0200

    Add Build.PL

--- Acme-Both-MakefilePL-And-BuildPL/lib/Acme/Both/MakefilePL/And/BuildPL.pm
+++ Acme-Both-MakefilePL-And-BuildPL/lib/Acme/Both/MakefilePL/And/BuildPL.pm
@@ -10,11 +10,11 @@ Acme::Both::MakefilePL::And::BuildPL - The great new Acme::Both::MakefilePL::And
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
